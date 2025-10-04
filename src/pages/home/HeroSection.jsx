ae0cb8f748953c414ac49410b127e2e22163dc77
Md.Rokonuzzaman Sayem <123319907+Abusayem99133@users.noreply.github.com>
2025-10-04 21:44:39 +0600

    Update HeroSection.jsx p tag text change

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -13,9 +13,7 @@ const HeroSection = () => {
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            my pc is stop because of power problem.
             </p>
             <button className="btn btn-primary">Get Started to button
             </button>
