c55af02743c2a126c315e475575af9d8afd11f8c
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-10-20 16:00:17 +0600

    updated to the subnavbar icon

--- src/components/shared/navbar/SubNavbar.jsx
+++ src/components/shared/navbar/SubNavbar.jsx
@@ -2,51 +2,46 @@ import logo from "../../../assets/banner/logo.png";
 const SubNavbar = () => {
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-sm">
-        <div className="dropdown dropdown-start">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className=" rounded-full">
-              <img alt="logo" src={logo} />
+      <div className="flex justify-between items-center ">
+        {/* div 1 */}
+        <div className="flex items-center">
+          <div className="dropdown dropdown-start">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className=" rounded-full">
+                <img alt="logo" src={logo} />
+              </div>
             </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
-        <div className=" flex-1">
           <div>
-            <a className="font-extrabold text-[24px] flex-col">Foodzy</a>
+            <h2 className="font-extrabold text-[24px] flex-col">Foodzy</h2>
+            <span className="font-semibold text-sm">A Treasure of Tastes</span>
           </div>
-          <span className="font-semibold text-sm">A Treasure of Tastes</span>
         </div>
-        <div className="flex gap-2">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          />
-        </div>
-        <div>
-          <h1>Account</h1>
-        </div>
+        {/* div 2 */}
+        <div>search bar</div>
+        {/* div 3 */}
+        <div>button</div>
       </div>
     </div>
   );
