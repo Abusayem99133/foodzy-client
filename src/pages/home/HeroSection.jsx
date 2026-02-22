ebeae3acb3e181a44b93d83d4488b26642b979a7
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2026-02-22 17:49:57 +0600

    updated to the banner section

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -1,22 +1,15 @@
+import banner from "../../assets/banner/green-plate-full-omelet-white-bread-with-pepper.jpg";
 const HeroSection = () => {
   return (
     <div>
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage:
-            "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-        }}
-      >
+      <div className="hero min-h-screen">
+        <img src={banner} alt="" />
         <div className="hero-overlay"></div>
         <div className="hero-content text-neutral-content text-center">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-            my pc is stop because of power problem.
-            </p>
-            <button className="btn btn-primary">Get Started to button
-            </button>
+            <p className="mb-5">my pc is stop because of power problem.</p>
+            <button className="btn btn-primary">Get Started to button</button>
           </div>
         </div>
       </div>
